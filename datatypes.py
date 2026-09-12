a3de1f57f01e1cbd8b1404bfdf20f37052559ee4
rayjadarajveersinh21 <rayjadarajveersinh21@gmail.com>
2026-09-12 08:47:05 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,75 @@
+#1. Demonstrate int, float, str, bool, and complex.
+
+# Integer
+age = 20
+
+# Float
+price = 99.50
+
+# String
+name = "Rajveersinh"
+
+# Boolean
+is_student = True
+
+# Complex
+number = 3 + 4j
+
+print("Integer:", age)
+print("Float:", price)
+print("String:", name)
+print("Boolean:", is_student)
+print("Complex:", number)
+
+
+#2. Accept two numbers and display their data types.
+num1 = 10
+num2 = 5.5
+
+print("First number:", num1)
+print("Data type:", type(num1))
+
+print("Second number:", num2)
+print("Data type:", type(num2))
+
+#3. Convert a string number into an integer and float.
+num = "25"
+
+integer_num = int(num)
+float_num = float(num)
+
+print("Integer:", integer_num)
+print("Float:", float_num)
+
+#4.
+text = "Hello Python"
+
+length = len(text)
+
+print("Length of string:", length)
+
+#5.
+my_list = [1, 2, 3]
+my_tuple = (1, 2, 3)
+my_set = {1, 2, 3}
+my_dict = {"name": "rajveersinh", "age": 20}
+
+print("List:", my_list)
+print("Type:", type(my_list))
+
+print("Tuple:", my_tuple)
+print("Type:", type(my_tuple))
+
+print("Set:", my_set)
+print("Type:", type(my_set))
+
+print("Dictionary:", my_dict)
+print("Type:", type(my_dict))
+
+
+
+
+
+
+
+
